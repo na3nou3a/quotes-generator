f3153b32b1540ac42aad5bbaf71b049a69d3c0cf
naima1515 <mirnanina2014@gmail.com>
2022-02-15 18:29:55 +0100

    Updates

---
+++ precache-manifest.a682e2949d27fc2415bef0e65e66523e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190252fb86969d039529b3e73ce62c0f",
+    "url": "/quotes-generator/index.html"
+  },
+  {
+    "revision": "73b9ec3b86441be24479",
+    "url": "/quotes-generator/static/css/2.709e72d0.chunk.css"
+  },
+  {
+    "revision": "fef334918e8c00fc3cb0",
+    "url": "/quotes-generator/static/css/main.c5c214da.chunk.css"
+  },
+  {
+    "revision": "73b9ec3b86441be24479",
+    "url": "/quotes-generator/static/js/2.5858aca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes-generator/static/js/2.5858aca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef334918e8c00fc3cb0",
+    "url": "/quotes-generator/static/js/main.4bc4a645.chunk.js"
+  },
+  {
+    "revision": "a8d467cb04feeaa900c0",
+    "url": "/quotes-generator/static/js/runtime-main.371c0485.js"
+  },
+  {
+    "revision": "7aadf21b202a6027ee170b7d25e21d48",
+    "url": "/quotes-generator/static/media/floral-g5aeb8fe4d_640.7aadf21b.jpg"
+  }
+]);
